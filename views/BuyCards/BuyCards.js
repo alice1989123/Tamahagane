d8867ff6aef8fd910f636d0e9fe82938358ad484
alice1989123 <aliciabasilo.ab@gmail.com>
2021-11-12 22:22:28 -0600

    Added Navigation component and dark-light mode

--- views/BuyCards/BuyCards.js
+++ views/BuyCards/BuyCards.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Grid, Box, Button } from "@chakra-ui/react";
+import { Grid, Box, Button, Flex, useColorModeValue } from "@chakra-ui/react";
 import styles from "../../styles/BuyCards.module.scss";
 import BuyModal from "./BuyModal";
 import React, { useState } from "react";
@@ -10,20 +10,23 @@ const BuyCards = function (props) {
   const [viewModal, setviewModal] = useState(false);
   const [buyOption, setbuyOption] = useState(0);
 
-  const latestBlock = props.latestBlock;
-
   return (
-    <>
-      <>
-        <Grid
-          templateColumns="repeat(auto-fit, minmax(300px, 1fr))"
-          grid-auto-rows="340px"
-          gap="2rem"
-        >
-          {buyOptions.map((i) => {
-            return (
-              <>
-                <Box className={styles.card} key={`pack${i}box`}>
+    <Flex flexDirection="column" justifyItems="center" marginTop={20}>
+      <Grid
+        templateColumns="repeat(auto-fit, minmax(300px, 1fr))"
+        grid-auto-rows="340px"
+        gap="2rem"
+        key={"gallery"}
+      >
+        {buyOptions.map((i) => {
+          return (
+            <Box
+              bg={useColorModeValue("gray.100", "gray.700")}
+              className={styles.card}
+              key={`pack${i}box`}
+            >
+              {
+                <>
                   <Image
                     src={`/pack${i}.png`}
                     width="350"
@@ -43,19 +46,22 @@ const BuyCards = function (props) {
                   >
                     {`Buy ${toWords[i]}  package`}
                   </Button>
-                </Box>
-              </>
-            );
-          })}
+                </>
+              }
+            </Box>
+          );
+        })}
 
+        {
           <BuyModal
             viewModal={viewModal}
             setviewModal={setviewModal}
             buyOption={buyOption}
+            key={"buyModal"}
           />
-        </Grid>
-      </>
-    </>
+        }
+      </Grid>
+    </Flex>
   );
 };
 
